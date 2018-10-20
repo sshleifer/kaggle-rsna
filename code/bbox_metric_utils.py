c7f0ee5c011d6e40d8e95404b3d5e2577ef8d5aa
sshleifer <sam@kensho.com>
2018-10-20 12:57:02 -0400

    metric utils and ensembling improvements

--- code/bbox_metric_utils.py
+++ code/bbox_metric_utils.py
@@ -22,7 +22,7 @@ def box_mask(box, shape=1024):
 # plt.imshow(box_mask([5,20,50,100], shape=256), cmap=mpl.cm.jet)
 
 # define function that extracts confidence and coordinates of boxes from a prediction mask
-def parse_boxes(msk, threshold=0.20, connectivity=None):
+def torch_mask_to_boxes(msk, threshold=0.20, connectivity=None):
     """
     :param msk: (torch.Tensor) CxWxH tensor representing the prediction mask
     :param threshold: threshold in the range 0-1 above which a pixel is considered a positive target
@@ -63,8 +63,7 @@ def parse_boxes(msk, threshold=0.20, connectivity=None):
 # print(dataset_train[3][1].shape)
 # print(parse_boxes(dataset_train[3][1]))
 
-# define function that creates prediction strings as expected in submission
-def prediction_string(predicted_boxes, confidences):
+def make_prediction_string(predicted_boxes, confidences):
     """
     :param predicted_boxes: [[x1, y1, w1, h1], [x2, y2, w2, h2], ...] list of predicted boxes
     coordinates
@@ -82,7 +81,7 @@ def prediction_string(predicted_boxes, confidences):
 # print(predicted_boxes, confidences)
 # print(prediction_string(predicted_boxes, confidences))
 
-# define iou function
+
 def IoU(pr, gt):
     """
     :param pr: (numpy_array(bool)) prediction array
@@ -114,8 +113,8 @@ def precision(tp, fp, fn):
 # # debug code for above function
 # print(precision(3,1,1))
 
-# define function that calculates the average precision of an image
-def average_precision_image(predicted_boxes, confidences, target_boxes, shape=1024):
+def average_precision_image(predicted_boxes, confidences, target_boxes, shape=1024,
+                            thresholds=np.arange(0.4, 0.75, 0.05)):
     """
     :param predicted_boxes: [[x1, y1, w1, h1], [x2, y2, w2, h2], ...] list of predicted boxes
     coordinates
@@ -128,51 +127,49 @@ def average_precision_image(predicted_boxes, confidences, target_boxes, shape=10
 
     # if both predicted and target boxes are empty, precision is NaN (and doesn't count towards
     # the batch average)
-    if predicted_boxes == [] and target_boxes == []:
+    if (len(predicted_boxes) + len(target_boxes)) == 0:
         return np.nan
-    else:
-        # if we have predicted boxes but no target boxes, precision is 0
-        if len(predicted_boxes) > 0 and target_boxes == []:
-            return 0.0
+    elif len(predicted_boxes) == 0:
+        return 0.0
         # if we have target boxes but no predicted boxes, precision is 0
-        elif len(target_boxes) > 0 and predicted_boxes == []:
-            return 0.0
+    elif len(target_boxes) == 0:
+        return 0.0
+
         # if we have both predicted and target boxes, proceed to calculate image average precision
-        else:
-            # define list of thresholds for IoU [0.4 , 0.45, 0.5 , 0.55, 0.6 , 0.65, 0.7 , 0.75]
-            thresholds = np.arange(0.4, 0.8, 0.05)
-            # sort boxes according to their confidence (from largest to smallest)
-            predicted_boxes_sorted = list(
-                reversed([b for _, b in sorted(zip(confidences, predicted_boxes),
-                                               key=lambda pair: pair[0])]))
-            average_precision = 0.0
-            for t in thresholds:  # iterate over thresholds
-                # with a first loop we measure true and false positives
-                tp = 0  # initiate number of true positives
-                fp = len(predicted_boxes)  # initiate number of false positives
-                for box_p in predicted_boxes_sorted:  # iterate over predicted boxes coordinates
-                    box_p_msk = box_mask(box_p, shape)  # generate boolean mask
-                    for box_t in target_boxes:  # iterate over ground truth boxes coordinates
-                        box_t_msk = box_mask(box_t, shape)  # generate boolean mask
-                        iou = IoU(box_p_msk, box_t_msk)  # calculate IoU
-                        if iou > t:
-                            tp += 1  # if IoU is above the threshold, we got one more true positive
-                            fp -= 1  # and one less false positive
-                            break  # proceed to the next predicted box
-                # with a second loop we measure false negatives
-                fn = len(target_boxes)  # initiate number of false negatives
+    else:
+        # define list of thresholds for IoU [0.4 , 0.45, 0.5 , 0.55, 0.6 , 0.65, 0.7 , 0.75]
+        # sort boxes according to their confidence (from largest to smallest)
+        predicted_boxes_sorted = list(
+            reversed([b for _, b in sorted(zip(confidences, predicted_boxes),
+                                           key=lambda pair: pair[0])]))
+        average_precision = 0.0
+        for t in thresholds:  # iterate over thresholds
+            # with a first loop we measure true and false positives
+            tp = 0  # initiate number of true positives
+            fp = len(predicted_boxes)  # initiate number of false positives
+            for box_p in predicted_boxes_sorted:  # iterate over predicted boxes coordinates
+                box_p_msk = box_mask(box_p, shape)  # generate boolean mask
                 for box_t in target_boxes:  # iterate over ground truth boxes coordinates
                     box_t_msk = box_mask(box_t, shape)  # generate boolean mask
-                    for box_p in predicted_boxes_sorted:  # iterate over predicted boxes coordinates
-                        box_p_msk = box_mask(box_p, shape)  # generate boolean mask
-                        iou = IoU(box_p_msk, box_t_msk)  # calculate IoU
-                        if iou > t:
-                            fn -= 1
-                            break  # proceed to the next ground truth box
-                # TBD: this algo must be checked against the official Kaggle evaluation method
-                # which is still not clear...
-                average_precision += precision(tp, fp, fn) / float(len(thresholds))
-            return average_precision
+                    iou = IoU(box_p_msk, box_t_msk)  # calculate IoU
+                    if iou > t:
+                        tp += 1  # if IoU is above the threshold, we got one more true positive
+                        fp -= 1  # and one less false positive
+                        break  # proceed to the next predicted box
+            # with a second loop we measure false negatives
+            fn = len(target_boxes)  # initiate number of false negatives
+            for box_t in target_boxes:  # iterate over ground truth boxes coordinates
+                box_t_msk = box_mask(box_t, shape)  # generate boolean mask
+                for box_p in predicted_boxes_sorted:  # iterate over predicted boxes coordinates
+                    box_p_msk = box_mask(box_p, shape)  # generate boolean mask
+                    iou = IoU(box_p_msk, box_t_msk)  # calculate IoU
+                    if iou > t:
+                        fn -= 1
+                        break  # proceed to the next ground truth box
+            # TBD: this algo must be checked against the official Kaggle evaluation method
+            # which is still not clear...
+            average_precision += precision(tp, fp, fn) / float(len(thresholds))
+        return average_precision
 
 
 # # debug code for above function
@@ -207,7 +204,7 @@ def average_precision_batch(output_batch, pIds, pId_boxes_dict, rescale_factor, 
             target_boxes = [[int(round(c / float(rescale_factor))) for c in box_t] for box_t in
                             target_boxes]
         # extract prediction boxes and confidences
-        predicted_boxes, confidences = parse_boxes(msk)
+        predicted_boxes, confidences = torch_mask_to_boxes(msk)
         batch_precisions.append(
             average_precision_image(predicted_boxes, confidences, target_boxes, shape=shape))
     if return_array:
@@ -215,41 +212,54 @@ def average_precision_batch(output_batch, pIds, pId_boxes_dict, rescale_factor, 
     else:
         return np.nanmean(np.asarray(batch_precisions))
 
-def compute_batch_ap(model, dataset, config, image_ids, preds_cache={},
-                     shape=256, thresh=0.95, just_stats=True):
+from mrcnn import utils
+def in_bounds_or_empty(r, shape):
+    if r.shape[0] == 0:
+        return True
+    else:
+        return r.max() <= shape and r.min() >= 0
+
+def compute_aps(detections, dataset, config, shape=256, thresh=0.95):
     import warnings
     warnings.filterwarnings('ignore')
     APs = []
-    dets = []
-    for image_id in tqdm_notebook(image_ids):
+    n_rois = []
+    image_ids = []
+    n_bbox = []
+    for image_id in tqdm_notebook(detections.keys()):
+        image_ids.append(image_id)
         # Load image
         image, image_meta, gt_class_id, gt_bbox, gt_mask = modellib.load_image_gt(
             dataset, config, image_id,
-            #    use_mini_mask=False
+            use_mini_mask=False
         )
-        if image_id not in preds_cache:
-            r = model.detect([image], verbose=0)[0]
-            preds_cache[image_id] = r
-        else:
-            r = preds_cache[image_id]
-        rois = r['rois'][np.where(r['scores'] > thresh)]
+        r = detections[image_id]
+        rois = r['rois'][np.where(r['scores'] > thresh)].astype(int)
         APs.append(
             average_precision_image(rois, r['scores'], gt_bbox, shape=shape)
         )
-        dets.append(r)
-    print("mAP @ IoU=50: {:.4f} ".format(np.mean(APs)))
-    if just_stats:
-        return APs
-    else:
-        return zip(image_ids, APs, dets)
+        n_rois.append(rois.shape[0])
+        n_bbox.append(gt_bbox.shape[0])
+    print("mAP {:.4f} ".format(np.nanmean(APs)))
+    print("mean n rois {:.2f} ".format(np.mean(n_rois)))
+    return pd.DataFrame({'ap': APs, 'n_roi': n_rois, 'n_bbox': n_bbox}, index=image_ids)
 
 
-def thresh_map_check(model, dataset_val, inference_config, n=6, start=.5):
+#res = {.5: .1417, .75: .1407, .85: .1365}
+def thresh_map_check(model, dataset_val, inference_config, n=6, start=.5, end=.99):
     cache = {}
-    for thresh in np.linspace(.5, 1., n=n):
-        res[thresh] = compute_batch_ap(model, dataset_val, inference_config,
+    res = {}
+    for thresh in np.linspace(start, end, n):
+        res[thresh] = compute_aps(model, dataset_val, inference_config,
                                        dataset_val.image_ids,
                                        preds_cache=cache,
                                        thresh=thresh, just_stats=True)
     return res
 
+import pandas as pd
+def aps_to_mean_ser(res):
+    return pd.Series({k: np.mean(v) if isinstance(v, list) else v for k,v in res.items()})
+
+def update_history(model):
+    for k,v in model.keras_model.history.history.items():
+        HISTORY[k] += v
